087a5466aae1c0b644d362eeaec1ce9d59e5afc2
Stepan Yershov <stepanersh@gmail.com>
2018-09-07 18:05:44 +0300

    add subscription service, refactor delete command

--- src/main/java/controller/command/DeleteSubscriptionCommand.java
+++ src/main/java/controller/command/DeleteSubscriptionCommand.java
@@ -1,15 +1,21 @@
 package controller.command;
 
 import model.service.SubscriptionService;
+import model.service.resource.manager.MessageManager;
 import model.service.resource.manager.PagePathManager;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.Locale;
 
 public class DeleteSubscriptionCommand implements Command {
     @Override
     public String execute(HttpServletRequest request) {
         int id = Integer.valueOf(request.getParameter("subId"));
-        new SubscriptionService().deleteUserSubscription(id);
+        if(! new SubscriptionService().deleteUserSubscription(id)){
+            throw new RuntimeException(
+                    new MessageManager((Locale)request.getSession().getAttribute("locale"))
+                            .getProperty("message.changes.not.accepted"));
+        }
         return new PagePathManager().getProperty("path.command.user.subscription")+"?state=UNPAID";
     }
 }
---
+++ src/test/java/model/service/SubscriptionServiceTest.java
@@ -0,0 +1,52 @@
+package model.service;
+
+import model.entity.DTO.PublicationDto;
+import model.entity.Subscription;
+import model.entity.User;
+import model.exception.NotEnoughMoney;
+import model.service.builders.PaymentBuilder;
+import model.service.builders.PublicationDtoBuilder;
+import model.service.builders.SubscriptionBuilder;
+import model.service.builders.UserBuilder;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+import static org.junit.Assert.*;
+
+public class SubscriptionServiceTest {
+    private static SubscriptionService service;
+
+    @BeforeClass
+    public static void init() {
+        service = new SubscriptionService();
+    }
+
+    @Test(expected = NotEnoughMoney.class)
+    public void GivenUserHaveNotEnoughMoneyWhenUserPaySubscriptionThenThrowException() {
+        User user = new UserBuilder()
+                .buildLogin("test")
+                .buildPassword("testik")
+                .buildEmail("test@test.com")
+                .buildFirstName("Test")
+                .buildLastName("Test")
+                .buildRole(User.RoleEnum.USER)
+                .buildAddress("Test, test, test, 13")
+                .buildAccount(new BigDecimal(15.5d))
+                .build();
+
+        Subscription subscription = new SubscriptionBuilder()
+                .buildStartDate(LocalDate.now())
+                .buildEndDate(LocalDate.now().plusMonths(1))
+                .buildOwnerId(user.getId())
+                .buildState(Subscription.StateEnum.UNPAID)
+                .buildPayment( new PaymentBuilder()
+                        .buildBill(new BigDecimal(54.7))
+                        .build())
+                .build();
+
+        service.paySubscription(user, subscription);
+    }
+}
